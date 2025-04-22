4a12c604b462ea80f4adb284761d5691cf366c34
Vinícius Freitas <viniciusfanjos9@gmail.com>
2025-04-22 19:03:23 -0300

    Add files via upload

---
+++ numeros1a50.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class numeros1a50 {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while(contador <= 50){
+            System.out.println("numero : " + contador);
+            contador++;
+        }
+
+    }
+}
