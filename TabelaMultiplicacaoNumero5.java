04d39baffb9f62d4d870f4c6e58e9d96f495782b
Vinícius Freitas <viniciusfanjos9@gmail.com>
2025-04-28 03:47:54 -0300

    Add files via upload

---
+++ TabelaMultiplicacaoNumero5.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class TabelaMultiplicacaoNumero5 {
+    public static void main(String[] args) {
+    int resultado;
+        for (int i = 1; i <=10;i++){
+            resultado = 5 * i;
+            System.out.println("5x"+ i + "=" + resultado);
+        }
+
+    }
+}
