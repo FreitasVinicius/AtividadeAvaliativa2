669e0a6bf86a00dd5c029ab762370e304f291e68
Vinícius Freitas <viniciusfanjos9@gmail.com>
2025-04-28 14:38:35 -0300

    Add files via upload

---
+++ calculoFatorial.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class calculoFatorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro positivo: ");
+        int numero = scanner.nextInt();
+
+        if (numero < 0) {
+            System.out.println("Número inválido! Por favor, digite um número inteiro positivo.");
+        } else {
+            int fatorial = 1;
+
+            for (int i = 1; i <= numero; i++) {
+                fatorial *= i;
+            }
+
+            System.out.println("O fatorial de " + numero + " é: " + fatorial);
+        }
+
+        scanner.close();
+
+    }
+}
